e2ef85bd61ce106038b53762d7eb6891ce233222
Yuri Piaza <yuripiazagomessantos@gmail.com>
2026-02-07 22:23:42 -0300

    correção bug imagens

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,13 @@
+import project1 from "'../public/pjadm.png"
+
 export const projects = [
-    {id: 1, project: 'Plataforma de Pedidos Completa', link:'https://peca-ja-adm.vercel.app/', img: '../public/pjadm.png' , description: 'Sistema completo de pedidos, com pagina de cliente e página do administrador. Feito em React, JavaScript, Tailwind CSS.'},
+    {id: 1, project: 'Plataforma de Pedidos Completa', link:'https://peca-ja-adm.vercel.app/', img: '/public/pjadm.png' , description: 'Sistema completo de pedidos, com pagina de cliente e página do administrador. Feito em React, JavaScript, Tailwind CSS.'},
 
-    {id: 2, project: 'Crivus digital', link:'https://crivusdigital.vercel.app/', img: '../public/crivus.png', description: 'Modelo de site institucional. Feito em React, JavaScript, Tailwind CSS'},
+    {id: 2, project: 'Crivus digital', link:'https://crivusdigital.vercel.app/', img: '/public/crivus.png', description: 'Modelo de site institucional. Feito em React, JavaScript, Tailwind CSS'},
     
-    {id: 3, project: 'Clone Spotify (Desktop)', link:'https://clonespotify-psi.vercel.app', img: '../public/spotify.png', description: 'Projeto clone do Spotify, com funcionalidades de busca e player. Feito em React, JavaScript, Tailwind CSS'},
+    {id: 3, project: 'Clone Spotify (Desktop)', link:'https://clonespotify-psi.vercel.app', img: '/public/spotify.png', description: 'Projeto clone do Spotify, com funcionalidades de busca e player. Feito em React, JavaScript, Tailwind CSS'},
 
-    {id: 3, project: 'Clone Spotify (Desktop)', link:'https://clonespotify-psi.vercel.app', img: '../public/spotify.png', description: 'Projeto clone do Spotify, com funcionalidades de busca e player. Feito em React, JavaScript, Tailwind CSS'},
+    {id: 3, project: 'Clone Spotify (Desktop)', link:'https://clonespotify-psi.vercel.app', img: '/public/spotify.png', description: 'Projeto clone do Spotify, com funcionalidades de busca e player. Feito em React, JavaScript, Tailwind CSS'},
 ]
 
 console.log(projects)
