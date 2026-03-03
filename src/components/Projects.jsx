71b553684519d7dcde131dc1c7ed411d89bb0bd9
Yuri Piaza <yuripiazagomessantos@gmail.com>
2026-03-02 22:41:59 -0300

    Atualização de projetos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,49 @@
 import { projects } from "../data/projects";
+
 import ProjectCard from "./ProjectCard"; // Importando o novo componente
 
+
+
 const Projects = () => {
+
   return (
+
     <section className="relative w-full py-20 px-6 md:px-16 min-h-screen">
-      
+
+     
+
       {/* Título Sticky (Fixo) */}
+
       <div className="sticky top-24 z-30 mb-24 pointer-events-none flex items-center justify-center">
+
         <h2 className="text-4xl md:text-3xl font-bold text-white">
+
           Projetos <span className="text-white/60 font-bold text-2xl md:text-3xl">Destaques</span>
-        </h2> 
+
+        </h2>
+
       </div>
 
+
+
       {/* Grid de Cards que chamam o componente individual */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 md:gap-32 relative z-10">
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 md:gap-5 relative z-10">
+
         {projects.map((item) => (
+
           <ProjectCard key={item.id} item={item} />
+
         ))}
+
       </div>
+
     </section>
+
   );
+
 };
+
+
 
 export default Projects;
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,12 @@
 
 export const projects = [
-    {id: 1, project: 'Plataforma de Pedidos Completa', link:'https://peca-ja-adm.vercel.app/', img: 'pjadm.png' , description: 'Sistema completo de pedidos, com pagina de cliente e página do administrador. Feito em React, JavaScript, Tailwind CSS.'},
-
+    {id: 1, project: 'AuraLab', link:'auralab-three.vercel.app', img: 'auralab.png', description: 'Modelo de landing page. Feito em React, JavaScript, Tailwind CSS'},
+    
     {id: 2, project: 'Crivus digital', link:'https://crivusdigital.vercel.app/', img: 'crivus.png', description: 'Modelo de site institucional. Feito em React, JavaScript, Tailwind CSS'},
     
-    {id: 3, project: 'Clone Spotify (Desktop)', link:'https://clonespotify-psi.vercel.app', img: 'spotify.png', description: 'Projeto clone do Spotify, com funcionalidades de busca e player. Feito em React, JavaScript, Tailwind CSS'},
-
-    {id: 3, project: 'Clone Spotify (Desktop)', link:'https://clonespotify-psi.vercel.app', img: 'spotify.png', description: 'Projeto clone do Spotify, com funcionalidades de busca e player. Feito em React, JavaScript, Tailwind CSS'},
+    {id: 3, project: 'Easy Stock Control', link:'https://easy-stock-control.vercel.app/', img: 'esc.png', description: 'Sistema de controle de estoque completo. Feito em React, JavaScript, Tailwind CSS'},
+    
+    {id: 4, project: 'Plataforma de Pedidos Completa', link:'https://peca-ja-adm.vercel.app/', img: 'pjadm.png' , description: 'Sistema completo de pedidos, com pagina de cliente e página do administrador. Feito em React, JavaScript, Tailwind CSS.'},
 ]
 
 console.log(projects)
